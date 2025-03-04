7436e5c5d800610f23a274106502ecb6b5f71191
Nainasingh0408 <naina.singh@resourcifi.com>
2025-03-04 15:41:33 +0530

    Update Shimmer.js

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,20 +1,20 @@
 const Shimmer=()=>{
     return(
         <div className="shimmer-container">
-            <div className="shimmer-cards">Cards</div> 
-            <div className="shimmer-cards">Cards</div> 
-            <div className="shimmer-cards">Cards</div> 
-            <div className="shimmer-cards">Cards</div> 
-            <div className="shimmer-cards">Cards</div> 
-            <div className="shimmer-cards">Cards</div> 
-            <div className="shimmer-cards">Cards</div> 
-            <div className="shimmer-cards">Cards</div> 
-            <div className="shimmer-cards">Cards</div> 
-            <div className="shimmer-cards">Cards</div>
-            <div className="shimmer-cards">Cards</div> 
-            <div className="shimmer-cards">Cards</div> 
-            <div className="shimmer-cards">Cards</div> 
-            <div className="shimmer-cards">Cards</div> 
+            <div className="shimmer-cards"></div> 
+            <div className="shimmer-cards"></div> 
+            <div className="shimmer-cards"></div> 
+            <div className="shimmer-cards"></div> 
+            <div className="shimmer-cards"></div> 
+            <div className="shimmer-cards"></div> 
+            <div className="shimmer-cards"></div> 
+            <div className="shimmer-cards"></div> 
+            <div className="shimmer-cards"></div> 
+            <div className="shimmer-cards"></div>
+            <div className="shimmer-cards"></div> 
+            <div className="shimmer-cards"></div> 
+            <div className="shimmer-cards"></div> 
+            <div className="shimmer-cards"></div> 
 
         </div>
     );
